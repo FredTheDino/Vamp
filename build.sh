f2999811d46517ba6ba981c86a100908c2928ad6
Ed T <edvard.thornros@gmail.com>
2018-09-14 14:14:03 +0200

    We now run and compile on Linux, it's fairly headache free once you figure it out...

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+cd bin
+cp -sf ../lib/Linux64/*.* .
+csc -out:vamp.mono -debug -nologo -r:FNA.dll ../src/*.cs
+
+if [[ $1 == "run" ]] 
+then
+	mono vamp.mono
+fi
+
+cd ..
---
+++ makefile
@@ -0,0 +1,8 @@
+_FLAGS =-debug -nologo -r:FNA.dll
+_SRC ="../src/Program.cs"
+_NAME =vamp.mono
+
+# TODO: Change to shell script. YAY...
+
+compile:
+	./build.sh
