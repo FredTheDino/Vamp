132318b5ba202ee6ec68064c7e29e49015dee820
Eddie-boi <Edvard.Thornros@gmail.com>
2018-09-16 22:34:56 +0200

    Started work on collisions. It's very rough.

--- src/Engine/Collider.cs
+++ src/Engine/Collider.cs
@@ -7,15 +7,26 @@ using System.Collections.Generic;
 
 namespace Vamp
 {
-	/*
-	 * Colliders that lets things hit other colliders.
-	 */
-
 	public enum Shape
 	{
 		Box,
 		Circle,
 	}
+
+	public class Overlap
+	{
+		public Collider a, b;
+		public float depth;
+		public Vector2 normal;
+
+		public Overlap(Collider a, Collider b)
+		{
+			this.a = a;
+			this.b = b;
+			this.normal = new Vector2();
+			this.depth = float.MaxValue;
+		}
+	};
 
 	public class Collider
 	{
@@ -29,31 +40,54 @@ namespace Vamp
 			// TODO: Add to the collision engine.
 		}
 
-		public bool overlaps(Collider other)
+		public float Project(Vector2 normal)
+		{
+			if (shape == Shape.Box)
+			{
+				if (Math.Abs(normal.X) > Math.Abs(normal.Y))
+				{
+					normal = new Vector2(Math.Sign(normal.X), 0);
+				}
+				else
+				{
+					normal = new Vector2(0, Math.Sign(normal.Y));
+				}
+
+				return Vector2.Dot(owner.Scale + owner.Position, normal);
+			}
+			else 
+			{
+				return Vector2.Dot(owner.Scale.X * normal + owner.Position, normal);
+			}
+		}
+
+		public Overlap overlaps(Collider other)
 		{
 			Vector2 distance = owner.Position - other.owner.Position;
-			Vector2 normalized_distance = Vector2.Normalize(distance);
-			List<Vector2> normals;
 
-			if (shape == other.Shape)
+			// Find all potential normals.
+			List<Vector2> normals = new List<Vector2>();
+			if (this.shape == Shape.Box || other.shape == Shape.Box)
 			{
-				normals = new List<Vector2>(new Vector2[] {
-						new Vector2( 1,  0), new Vector2( 0,  1),
-						new Vector2(-1,  0), new Vector2( 0, -1)
+				normals.AddRange(new Vector2[] {
+					new Vector2( 1,  0), new Vector2( 0,  1)
 				});
 			}
-
-			if (shape == Shape.Box && other.Shape == Shape.Box)
+			if (this.shape == Shape.Circle || other.shape == Shape.Circle)
 			{
-				
-			}
-			else
-			{
-				Console.WriteLine("SHOULD NOT REACH HERE");
-				return false;
+				normals.Add( Vector2.Normalize(distance) );
 			}
 
-			return false;
+			Overlap overlap = new Overlap(this, other);
+			foreach (Vector2 normal in normals)
+			{
+				// Get extreems (Different signs of the normals).
+				// Check if they overlap with distance projected.
+				// If they don't, return false, else keep looping until
+				// we run out of normals.
+			}
+
+			return overlap;
 		}
 
 		// Dumb Getter and Setter methods
@@ -63,19 +97,18 @@ namespace Vamp
 			{
 				return owner; 
 			}
-
 			set 
 			{
 				owner = value; 
 			}
 		}
+
 		public Shape Shape 
 		{
 			get
 			{
 				return shape; 
 			}
-
 			set 
 			{
 				shape = value; 
