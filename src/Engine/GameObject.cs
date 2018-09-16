0b65f9c328be1472db92727a163c08a742119108
Erik Mattfolk <erik.mattfolk@gmail.com>
2018-09-16 15:19:28 +0200

    Added comments to GameObject

--- src/Engine/GameObject.cs
+++ src/Engine/GameObject.cs
@@ -6,14 +6,25 @@ using Microsoft.Xna.Framework.Input;
 
 namespace Vamp
 {
+    /*
+     * Main class for all objects in the game
+     */
     public abstract class GameObject
     {
+        // The attached collider
         private Collider collider;
+
+        // The position of the object
         private Vector2 position;
+
+        // The size and scale of the object
         private float size, scale;
 
+
+        // Base constructor
         public GameObject () : this(new Vector2(), 128, 1, null) {}
 
+        // Main constructor
         public GameObject (Vector2 position, float size, float scale, Collider collider)
         {
             this.position = position;
@@ -22,6 +33,8 @@ namespace Vamp
             this.collider = collider;
         }
 
+
+        // Getter and Setter methods
         public Collider Collider { get => collider; set => collider = value; }
         public Vector2 Position { get => position; set => position = value; }
         public float Scale { get => scale; set => scale = value; }
