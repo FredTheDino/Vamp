f5e9c7404ac797afca51ce28f13a773b4bc9ad2d
Ed T <edvard.thornros@gmail.com>
2018-09-17 20:39:13 +0200

    Tracking a bug with the colliders.

--- src/Engine/Collider.cs
+++ src/Engine/Collider.cs
@@ -43,33 +43,29 @@ namespace Vamp
 			Vector2 distance = A.Position - B.Position;
 
 			// Find all potential normals.
-			List<Vector2> normals = new List<Vector2>();
-			if (A.Collider.Shape == Shape.Box || B.Collider.Shape == Shape.Box)
-			{
-				normals.AddRange(new Vector2[] {
-					new Vector2( 1,  0), new Vector2( 0,  1)
-				});
-			}
-			if (A.Collider.Shape == Shape.Circle || B.Collider.Shape == Shape.Circle)
-			{
-				normals.Add( Vector2.Normalize(distance) );
-			}
+			Vector2[] normals = new Vector2[] {
+				new Vector2( 1,  0), new Vector2( 0,  1),
+				Vector2.Normalize(distance)
+			};
 
 			Overlap overlap = new Overlap(A, B);
+			Console.WriteLine("----------");
 			foreach (Vector2 Normal in normals)
 			{
 				float projected_distance = 
 					Math.Abs(Vector2.Dot(distance, Normal));
 				float projected_limit = 
-					Math.Abs(B.Collider.Project(B.Scale, Normal)) + 
-					Math.Abs(A.Collider.Project(A.Scale, Normal));
+					Math.Abs(B.Collider.Project(B.Scale,  Normal)) + 
+					Math.Abs(A.Collider.Project(A.Scale, -Normal));
 				float Depth = projected_limit - projected_distance;
 
 				// They overlap.
 				if (Depth < overlap.Depth)
 				{
+					// Can this be refactored.
 					overlap.Normal = Vector2.Dot(Normal, distance) < 0 ? -Normal : Normal;
 					overlap.Depth = Depth;
+					Console.WriteLine($"{overlap.Normal}, {Depth}");
 				}
 
 				if (Depth < 0.0)
@@ -93,23 +89,17 @@ namespace Vamp
 
 		public float Project(Vector2 Scale, Vector2 Normal)
 		{
+			float result;
 			if (shape == Shape.Box)
 			{
-				if (Math.Abs(Normal.X) > Math.Abs(Normal.Y))
-				{
-					Normal = new Vector2(Math.Sign(Normal.X), 0);
-				}
-				else
-				{
-					Normal = new Vector2(0, Math.Sign(Normal.Y));
-				}
-
-				return Vector2.Dot(Scale, Normal);
+				result = Vector2.Dot(Scale, 
+						new Vector2(Math.Sign(Normal.X), Math.Sign(Normal.Y)));
 			}
 			else 
 			{
-				return Vector2.Dot(Scale.X * Normal, Normal);
+				result = Vector2.Dot(Scale.X * Normal, Normal);
 			}
+			return result;
 		}
 
 		public Shape Shape 
